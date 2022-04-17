85f18e69304702f749f50a85fb1b3f0f6c4e3d82
Xavier TOLZA <tolza.xavier@gmail.com>
2022-04-17 15:25:17 +0200

    wip parsing

--- lbp/lbp.py
+++ lbp/lbp.py
@@ -1,21 +1,42 @@
+from base64 import b64decode
+from datetime import datetime
+from hashlib import sha256
+from time import sleep, time
+
+from selenium.common.exceptions import (ElementNotInteractableException,
+                                        NoSuchElementException,
+                                        StaleElementReferenceException)
 from selenium.webdriver.remote.webdriver import BaseWebDriver
-from selenium.common.exceptions import StaleElementReferenceException
+
+from lbp.constants import digits_base_64
 from lbp.frame_context import FrameContext
+
 
 class LBP(object):
     base_url = "https://www.labanquepostale.fr"
-    def __init__(self, driver:BaseWebDriver, user_id:str, user_pwd:str) -> None:
+    def __init__(self, driver:BaseWebDriver, user_id:str, user_pwd:str, wait_item_timeout=3) -> None:
         self.driver = driver
+        self.wait_item_timeout = wait_item_timeout
         self.__login = (user_id, user_pwd)
         
     def get_home(self) -> None:
         self.driver.get(self.base_url)
         
     def __getitem__(self, key):
-        if len(key) and key[0]=="#":
-            return self.driver.find_element_by_css_selector(key)
-        else:
-            return self.driver.find_elements_by_css_selector(key)
+        t0 = time()
+        while True:
+            try:
+                if len(key) and key[0]=="#" and " " not in key:
+                    return self.driver.find_element_by_css_selector(key)
+                else:
+                    res = self.driver.find_elements_by_css_selector(key)
+                    if len(res)==0:
+                        raise NoSuchElementException(key)
+                    return res
+            except (StaleElementReferenceException, NoSuchElementException) as error:
+                if time()-t0 > self.wait_item_timeout:
+                    raise TimeoutError(f"Timeout while waiting for {key}") from error
+                sleep(0.1)
     
     def frame_context(self, frame_id:str) -> FrameContext:
         return FrameContext(self.driver, frame_id)
@@ -30,11 +51,38 @@ class LBP(object):
     
     @property
     def digicode_buttons(self):
-        return [self[f"val_cel_{i}"] for i in range(4*4)]
-    
+        while True:
+            res = {}
+            for i in (self[f"#val_cel_{i}"] for i in range(4*4)):
+                hasher = sha256()
+                hasher.update(i.screenshot_as_png)
+                digest = hasher.hexdigest()[:6]
+                if digest in digits_base_64:
+                    res[digits_base_64[digest]] = i
+            
+            if len(res) != 10:
+                sleep(0.1)
+                continue
+            return res
+            
     def __enter_password(self) -> None:
         buttons = self.digicode_buttons
-        raise NotImplementedError()
+        for char in map(int,str(self.__login[1])):
+            buttons[char].click()
+    
+    def send_keys_secure(self, item, keys:str, timeout:float=3) -> None:
+        """
+        Send keys to the item provided and retry if the item is not interractable
+        """
+        t0 = time()
+        while True:
+            try:
+                item.send_keys(keys)
+                break
+            except (StaleElementReferenceException, ElementNotInteractableException) as error:
+                if time()-t0 > timeout:
+                    raise TimeoutError(f"Timeout while waiting for {item}") from error
+                sleep(0.1)
         
     def login(self) -> None:
         self.get_home()
@@ -58,13 +106,42 @@ class LBP(object):
         
         # Enter login
         with self.frame_context(connexion_iframe):
-            self["#val_cel_identifiant"].send_keys(self.__login[0])
+            user_field = self["#val_cel_identifiant"]
+            self.send_keys_secure(user_field, self.__login[0])
             # Enter password
             self.__enter_password()
+            self["#valider"].click()
+            
+    def parse_current_contract(self):
+        header = self["#form_liste_comptes h2 span"]
+        sold_date,sold = [i.text for i in self["#form_liste_comptes div.infos-cpt>span"]]
+        res = {
+            "owner":header[-1].text,
+            'account_id':header[0].text.split(" ")[-1],
+            "amount_date": datetime.strptime(sold_date.split(" ")[-1], "%d/%m/%Y"),
+            "amount": float(sold.split(" ")[1].replace(",",".")),
+        }
+        return res
+    
+    
+            
+    def dump_all_data(self):
+        # Going to contracts menu
+        self["#menuPrincipalNavigation li"][0].click()
+        n_contracts = len(self.contracts_buttons)
+        contracts = []
+        for contract_index in range(n_contracts):
+            self.contracts_buttons[contract_index].click()
+            contracts.append(self.parse_current_contract())
             raise NotImplementedError()
+
+    @property
+    def contracts_buttons(self):
+        return self["ul.listeDesCartouches li div.account-data div.title h3"]
         
     def __enter__(self):
         self.login()
+        return self
         
     def __exit__(self, *exc_info, **kwargs):
         self.driver.close()
